5af2db0327755fa5f02b6c65d83ec3008997a83f
Jan Schnasse <schnasse@gmx.de>
2020-04-19 22:22:34 +0200

    create a test suite to make coverage more realistic

---
+++ src/test/java/IntegrationTests.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.schnasse.oi.CsvTest;
+import org.schnasse.oi.JsonTest;
+import org.schnasse.oi.XmlTest;
+import org.schnasse.oi.YmlTest;
+import org.schnasse.oi.helper.UrlUtilTest;
+import org.schnasse.oi.main.MainTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({CsvTest.class,JsonTest.class,XmlTest.class,YmlTest.class,UrlUtilTest.class,MainTest.class})
+public class IntegrationTests {
+
+}
