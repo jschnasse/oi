6a8d90387b43872f36f18eb0806042bc204bb8e6
Jan Schnasse <schnasse@gmx.de>
2020-05-08 13:25:27 +0200

    add unit test to config

--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -1,9 +1,10 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.schnasse.oi.writer.XmlWriterUnitTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({ XmlWriterUnitTest.class })
 public class UnitTests {
 
 }
