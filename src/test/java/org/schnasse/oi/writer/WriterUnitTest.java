2d317f749a1382d1093c1858e8acd7c7defa882e
Jan Schnasse <schnasse@gmx.de>
2020-05-08 13:22:17 +0200

    first unit test

---
+++ src/test/java/org/schnasse/oi/writer/WriterUnitTest.java
@@ -0,0 +1,26 @@
+package org.schnasse.oi.writer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class WriterUnitTest {
+	protected final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+
+	@After
+	public void restoreStreams() {
+		System.setOut(originalOut);
+		System.setErr(originalErr);
+	}
+}
---
+++ src/test/java/org/schnasse/oi/writer/XmlWriterUnitTest.java
@@ -0,0 +1,26 @@
+package org.schnasse.oi.writer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.junit.Test;
+import org.schnasse.oi.reader.JsonReader;
+
+public class XmlWriterUnitTest extends WriterUnitTest {
+
+	@Test
+	public void testXmlWriter() throws IOException {
+		try (InputStream input = new ByteArrayInputStream(new String("{\"root\": \"test\"}").getBytes())) {
+			Map<String, Object> map = JsonReader.getMap(input);
+			XmlWriter.gprint(map);
+			assertEquals(
+					"<?xml version='1.1' encoding='UTF-8'?>\n<LinkedHashMap>\n  <root>test</root>\n</LinkedHashMap>"
+							.trim(),
+					outContent.toString().trim());
+		}
+	}
+}
