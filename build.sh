03da4462cc5bb6939fdb377003cdea68c468bede
Jan Schnasse <schnasse@gmx.de>
2020-05-22 11:49:10 +0200

    add bintray script - support setting of procname for better visibility in top

--- build.sh
+++ build.sh
@@ -2,21 +2,37 @@
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $scriptdir
+source VERSIONS
+mvnparam=$1
 
-package=oi_0.0.1
-
-function build(){
+function build_oi(){
+ package_name=$1
+ package_version=$2
+ package=${package_name}_$package_version
  mkdir -p deb/$package/usr/lib
  mkdir -p deb/$package/usr/bin
  mkdir -p deb/$package/usr/share/man/man1/
- mvn package $1
- sudo cp src/main/resources/oi deb/$package/usr/bin
- sudo cp target/oi.jar deb/$package/usr/lib
- cd $scriptdir/man
- asciidoctor -b manpage man.adoc
- cd -
- sudo cp $scriptdir/man/oi.1 deb/$package/usr/share/man/man1/
+ mvn package -D$mvnparam
+ sudo cp src/main/resources/$package_name deb/$package/usr/bin
+ sudo cp target/$package_name.jar deb/$package/usr/lib
 }
 
-build $1
-dpkg-deb --build deb/$package
+function build(){
+ package_name=$1
+ package_version=$2
+ package=${package_name}_$package_version
+
+ if [ -f $scriptdir/man/$package_name ]
+ then
+   cd $scriptdir/man/$package_name
+   asciidoctor -b manpage man.adoc
+   cd -
+   sudo cp $scriptdir/man/$package_name/$package_name.1 deb/$package/usr/share/man/man1/
+ fi  
+ dpkg-deb --build deb/$package
+}
+
+build_oi oi $oi_version
+build oi $oi_version
+build lscsv $lscsv_version
+build libprocname $libprocname_version
---
+++ push_to_bintray.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source VERSIONS
+
+function push_to_bintray(){
+cd $scriptdir
+PACKAGE=$1
+VERSION=$2
+API_AUTH=$3
+subject=jschnasse
+repo=debian
+filepath=${PACKAGE}_${VERSION}.deb
+curl -u$API_AUTH -XPOST "https://bintray.com/api/v1/packages/$subject/$repo/" -d@bintray/${PACKAGE}/package.json -H"content-type:application/json"
+curl -u$API_AUTH -XPOST "https://bintray.com/api/v1/packages/$subject/$repo/$PACKAGE/versions" -d@bintray/${PACKAGE}/version.json -H"content-type:application/json"
+curl -u$API_AUTH -T deb/$filepath "https://bintray.com/api/v1/content/$subject/$repo/$PACKAGE/$VERSION/$filepath;deb_distribution=buster;deb_component=main;deb_architecture=all;publish=1;override=1;"
+#curl -u$API_AUTH -XPUT "https://bintray.com/api/v1/content/$subject/$repo/$PACKAGE/$VERSION/$filepath" -d'{"list_in_downloads":true}'
+curl -u$API_AUTH -XPUT "https://bintray.com/api/ui/artifact/$subject/$repo/$filepath" -d'{"list_in_downloads":true}' -H"content-type:application/json"
+cd -
+}
+apiauth=$1
+push_to_bintray oi $oi_version $apiauth
+push_to_bintray lscsv $lscsv_version $apiauth
+push_to_bintray libprocname $libprocname_version $apiauth
